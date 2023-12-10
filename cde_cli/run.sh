ee7a104203dd099cbfcf4554096854aba943b105
Maximilian Engelhardt <mengelhardt@cloudera.com>
2023-12-10 18:19:15 +0100

    docker configs

--- cde_cli/run.sh
+++ cde_cli/run.sh
@@ -4,4 +4,4 @@ set -e
 cd "$(dirname "$0")"
 
 IMAGE=maxhardt90/cde-cli:latest
-docker run -v ./config:/home/cdeuser/config --env-file ./config/env.cfg -it $IMAGE bash
+docker run -v ./config:/home/cdeuser/.cde -it $IMAGE bash
