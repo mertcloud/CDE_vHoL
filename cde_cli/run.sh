19e53286360011a13e68a65edb2ff83cd99643e7
Maximilian Engelhardt <mengelhardt@cloudera.com>
2023-12-12 09:22:24 +0100

    docker abs path

--- cde_cli/run.sh
+++ cde_cli/run.sh
@@ -4,4 +4,4 @@ set -e
 cd "$(dirname "$0")"
 
 IMAGE=maxhardt90/cde-cli:latest
-docker run -v ./config:/home/cdeuser/.cde -it $IMAGE bash
+docker run -v ${PWD}/config:/home/cdeuser/.cde -it $IMAGE bash
